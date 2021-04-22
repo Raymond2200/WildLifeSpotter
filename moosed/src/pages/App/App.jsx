6625249bbcd5fe1c35f08680f5c28606cd78f544
Logan <mlogan858@hotmail.com>
2021-04-22 17:55:55 -0400

    google maps

--- moosed/src/pages/App/App.jsx
+++ moosed/src/pages/App/App.jsx
@@ -1,11 +1,28 @@
 import './App.css';
+import Map from '../../components/Map/Map'
+import { Component } from 'react'
+import { getCurrentLatLng} from '../../services/geolocation'
 
-function App() {
-  return (
-    <div className="App">
-      Moosed
-    </div>
-  );
+class App extends Component {
+  state = {
+    lat: null,
+    lng: null,
+  }
+  async componentDidMount () {
+    const {lat, lng } = await getCurrentLatLng()
+    this.setState({ lat: lat, lng: lng})
+  }
+  render() {
+    return (
+      <div className="App">
+        Moosed
+        <Map 
+          lng={this.state.lng}
+          lat={this.state.lat}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
